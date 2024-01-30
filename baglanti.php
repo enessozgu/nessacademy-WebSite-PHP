356183666ebacad1990e392b9e369a3ce7c52fc9
Enes <ozguenes196@gmail.com>
2024-01-31 00:10:05 +0300

    commit

--- baglanti.php
+++ baglanti.php
@@ -7,16 +7,17 @@
 // $dbname = "nese7cademcomtr_db";
  
  $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "test";
-
- $conn=mysqli_connect($servername,$username,$password,$dbname);
-
- if(!$conn){
-    die("Bağlantı Kurulamadı :" .mysqli_connect_error());
-
- }
+ $username = "nese7cademcomtr_enes";
+ $password = "rT-XR8l2%gkG";
+ $dbname = "nese7cademcomtr_db";
+ 
+  $conn=mysqli_connect($servername,$username,$password,$dbname);
+ 
+  if(!$conn){
+     die("Bağlantı Kurulamadı :" .mysqli_connect_error());
+ 
+  }
+ 
 
 
  
